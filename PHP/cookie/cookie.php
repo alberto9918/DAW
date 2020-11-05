a30da2dc53b649707304dc220cc26a7875a04608
Alberto <alberto.santiago.rodriguez.alu@iesjulioverne.es>
2020-11-05 14:20:57 +0100

    Cookies PHP

---
+++ PHP/cookie/cookie.php
@@ -0,0 +1,23 @@
+<?php
+    setcookie("font-size", "40px", time() + 60*60*24*30);
+    /*
+        if(isset($_COOKIE['font-size']){
+            $tam = $_COOKIE['font-size'];
+        })else{
+            setcookie("font-size", "40px", time() + 60*60*24*30);
+        }
+    */ 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
