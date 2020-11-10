5b92b40e2179196651a33991d0ce5654bc1148ae
Alberto <alberto.santiago.rodriguez.alu@iesjulioverne.es>
2020-11-10 10:03:36 +0100

    controlador buscar usuario

--- DWEBS/A13_Servlet/target/classes/controlador/control_Usuario/Controlador_BuscarUsuario.java
+++ DWEBS/A13_Servlet/target/classes/controlador/control_Usuario/Controlador_BuscarUsuario.java
@@ -1,11 +1,17 @@
 package controlador.control_Usuario;
 
 import java.io.IOException;
+
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import DAO.UsuarioDAO;
+import modelo.Usuario;
 
 /**
  * Servlet implementation class Controlador_BuscarUsuario
@@ -13,10 +19,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Controlador_BuscarUsuario")
 public class Controlador_BuscarUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
+	@Resource(lookup="jdbc/MVC_JSP")
+    private DataSource miPool;
+	
+	private UsuarioDAO usuarioDAO;
+	
     public Controlador_BuscarUsuario() {
         super();
         // TODO Auto-generated constructor stub
@@ -26,8 +34,28 @@ public class Controlador_BuscarUsuario extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			usuarioDAO = new UsuarioDAO(miPool);
+			
+			//validación
+			Long id = Long.parseLong(request.getParameter("idUsuario"));
+			
+			Usuario u = usuarioDAO.read(id);
+			System.out.println("el id es: " + id + " " + u);
+			
+			if(u != null) {
+				request.setAttribute("usuarioBuscado", u);
+				request.getRequestDispatcher("vistasUsuario/mostrarUsuario.jsp").forward(request, response);
+			}else {
+				request.getRequestDispatcher("vistasUsuario/error.jsp").forward(request, response);
+			}
+		}catch(NumberFormatException e1){
+			request.getRequestDispatcher("vistasUsuario/error.jsp").forward(request, response);
+		}catch(Exception e1) {
+			throw new ServletException();
+		}
+		
 	}
 
 	/**
