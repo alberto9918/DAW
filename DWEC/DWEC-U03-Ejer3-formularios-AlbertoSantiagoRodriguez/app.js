45a9c99e35d7c6de1cd3e296f1c78e1cd78508e4
Alberto <albertosantiagorodriguez@gmail.com>
2020-11-27 08:12:40 +0100

    ejercicio validar form

--- DWEC/DWEC-U03-Ejer3-formularios-AlbertoSantiagoRodriguez/app.js
+++ DWEC/DWEC-U03-Ejer3-formularios-AlbertoSantiagoRodriguez/app.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-        document.getElementById("btnEnviar").addEventListener('click', validarFormulario); 
+        document.formulario.addEventListener('submit', validarFormulario); 
         document.getElementById("nombre").addEventListener('blur', ponerMayuscula);
         document.getElementById("apellidos").addEventListener('blur', ponerMayuscula);
 }, false);
@@ -9,34 +9,86 @@ var incremento = 0;
 
 function validarFormulario(evento) {
         evento.preventDefault();
-        var errores = document.getElementById('errores');
-        var intentos = document.getElementById('intentos');
         incremento++;
 
-        var nombre = document.getElementById('nombre').value;
-        var apellidos = document.getElementById('apellidos').value;
+        //Array para guardar los mensajes de los campos que no pasan la validación.
+        var campos = [];
+
+        var errores = document.getElementById('errores');
+        var intentos = document.getElementById('intentos');
+
+        //Esto lo hago para borrar todos los mensajes de error que hayan salido. Así, consigo que una vez validado correctamente 
+        //el formulario, se borren los mensajes de error que puedan haber salido, y además evito que se repitan en caso de que 
+        //no se complete un campo de forma correcta de forma reiterada.
+        errores.innerHTML = "";
+
+        var nombre = document.getElementById('nombre');
+        var apellidos = document.getElementById('apellidos');
         var edad = document.getElementById('edad');
-        var email = document.getElementById('nif').value;
-        var email = document.getElementById('email').value;
-        var provincia = document.getElementById('provincia').value;
-        var telefono = document.getElementById('fecha').value;
+        var dni = document.getElementById('nif');
+        var email = document.getElementById('email');
+        var provincia = document.getElementById('provincia');
+        var fecha = document.getElementById('fecha');
+        var telefono = document.getElementById('telefono');
         var hora = document.getElementById('hora').value;
 
-        if(nombre.length == 0) {
-                var nodo = document.createElement("p");
-                nodo.setAttribute("id","nombre")
-                nodo.innerHTML = "El nombre no puede estar vacío";
+        nombre.setAttribute("class","");
 
-                errores.appendChild(nodo);
+        if(nombre.value.length == 0) {
+                campos['nombre'] = 'El nombre no puede estar vacío';
+                nombre.setAttribute("class","error");
         }
 
-        if(edad.getAttribute('type') == 'texto'){
-                edad.setAttribute('type') = 'number';
+        if(apellidos.value.length == 0){
+                campos['apellidos'] = 'Los apellidos no pueden estar vacíos';
+                apellidos.setAttribute("class","error");
         }
 
-        if(errores.hasChildNodes()) { 
+        if(edad.type == 'text'){
+                campos['edad'] = 'La edad solo puede contener números.';
+
+                edad.type = 'number';
+                edad.max = 105;
+                edad.min = 0;
+                edad.value = 0;
+
+                edad.setAttribute("class","error");
+        }
+
+        if(!comprobarNIF(dni.value)){
+                campos['dni'] = 'El DNI introducido no es válido.';
+                dni.setAttribute("class","error");
+        }
+
+        if(!comprobarCorreo(email.value)){
+                campos['correo'] = 'El correo introducido no es válido.';
+                email.setAttribute("class","error");
+        }
+
+        if(provincia.value == 0){
+                campos['provincia'] = 'No ha seleccionado una provincia.';
+                provincia.setAttribute("class","error");
+        }
+
+        if(!comprobarTelefono(telefono.value)){
+                campos['telefono'] = 'El teléfono introducido no es válido.';
+                telefono.setAttribute("class","error");
+        }
+
+        
+        //Con este bloque se indica que en el caso de que el array de campos no esté vacío, es decir,
+        //que existan errores en la validación, se muestren todos los mensajes de error guardados en 
+        //ese array, y que no permita enviar el formulario.
+        if(Object.keys(campos).length !== 0){
+                for (var campo in campos) {
+                        let nodo = document.createElement("p");
+                        nodo.innerHTML = campos[campo];
+
+                        errores.appendChild(nodo);
+                }
                 return;
         }
+        
         
         
         this.submit();
@@ -45,9 +97,6 @@ function validarFormulario(evento) {
 function ponerMayuscula(){
         var primerEspacio = /^ /;
         var ultimoEspacio = / $/;
-
-        var nombre = document.getElementById('nombre');
-        var apellidos = document.getElementById('apellidos');
 
         nombre.value = nombre.value.replace (primerEspacio,"");
         nombre.value = nombre.value.replace (ultimoEspacio,"");
@@ -58,4 +107,45 @@ function ponerMayuscula(){
         
         apellidos.value = apellidos.value.charAt(0).toUpperCase() + apellidos.value.slice(1); 
         
+}
+
+function comprobarNIF(nif) {
+        var numero;
+        var letra;
+        var comprobarLetra;
+        var expresion_regular_nif
+       
+        expresion_regular_nif = /^\d{8}-[a-zA-Z]$/;
+       
+        if(expresion_regular_nif.test(nif) == true){
+                numero = nif.substr(0,nif.length-3);
+                letra = nif.substr(nif.length-1,1);
+                numero = numero % 23;
+                comprobarLetra='TRWAGMYFPDXBNJZSQVHLCKET';
+                comprobarLetra=comprobarLetra.substring(numero,numero+1);
+
+                if (letra!=letra.toUpperCase()) {
+                        return false;
+                }else{
+                        return true;
+                }
+        }else{
+                return false;
+        }
+}
+
+function comprobarCorreo(correo){
+        if(/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,4})$/.test(correo) == true){
+                return true;
+        }else{
+                return false;
+        }
+}
+
+function comprobarTelefono(telefono){
+        if(/^\d{9}$/.test(telefono) == true){
+                return true;
+        }else{
+                return false;
+        }
 }
